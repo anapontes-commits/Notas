fa5b7fd307fff620cf03e285237b83fbd0706d87
ana <anamariapontesferreira15@gmail.com>
2026-01-02 23:50:12 -0300

    Add files via upload

---
+++ .c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    float n1, n2, media;
+
+    printf("Nota 1: ");
+    scanf("%f", &n1);
+
+    printf("Nota 2: ");
+    scanf("%f", &n2);
+
+    media = (n1 + n2) / 2;
+
+    printf("Média: %.2f\n", media);
+
+    if (media >= 6)
+        printf("Situação: Aprovado\n");
+    else
+        printf("Situação: Reprovado\n");
+
+    return 0;
+}
